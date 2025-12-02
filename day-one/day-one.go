950f937c27f823bb2b77a20bc1f68e093f9357bf
Dirk <dg.schudde@live.nl>
2025-12-02 13:43:39 +0100

    Day 1 part #1

---
+++ day-one/day-one.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"advent-of-code-2025/common"
+	"fmt"
+	"strconv"
+)
+
+var outComes map[int]int
+
+func main() {
+	var input *[]string
+
+	input = common.ReadInput("./input/input.txt")
+
+	partOne(*input)
+}
+
+func partOne(input []string) {
+	var dial = 50
+
+	outComes = make(map[int]int, len(input))
+
+	for index, element := range input {
+		var direction = string(element[0])
+		var distance, _ = strconv.Atoi(element[1:])
+
+		distance = distance % 100
+
+		switch direction {
+		case "R":
+			{
+				dial += distance
+				if dial > 99 {
+					dial = dial - 100
+				}
+			}
+		case "L":
+			{
+				dial -= distance
+				if dial < 0 {
+					dial = dial + 100
+				}
+			}
+		}
+		//fmt.Printf("Direction: %s => %d dial: %d\r\n", direction, distance, dial)
+		outComes[index] = dial
+	}
+
+	var occurrences int = 0
+	for _, number := range outComes {
+		if number == 0 {
+			occurrences++
+		}
+	}
+
+	fmt.Printf("Password: %d\r\n", occurrences)
+}
